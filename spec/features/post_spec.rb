672dc2fac408db58c7b71d2b4164918c35196af0
gabe <gj+code@mail.co.de>
2016-08-14 22:01:46 -0400

    reorder tests; a --> an; improve desc of test #2

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -5,14 +5,14 @@ describe 'navigate' do
     @post = Post.create(title: "My Post", description: "My post desc")
   end
 
-  it 'shows the title on the show page in a h1 tag' do
-    visit "/posts/#{@post.id}"
-    expect(page).to have_css("h1", text: "My Post")
-  end
-
-  it 'to post pages' do
+  it 'loads the show page' do
     visit "/posts/#{@post.id}"
     expect(page.status_code).to eq(200)
+  end
+
+  it 'shows the title on the show page in an h1 tag' do
+    visit "/posts/#{@post.id}"
+    expect(page).to have_css("h1", text: "My Post")
   end
 
   it 'shows the description on the show page in a p tag' do
