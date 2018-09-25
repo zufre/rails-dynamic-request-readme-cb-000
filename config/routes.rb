91530fc2d7d4cc58e763cea5451df13d0d765242
Cedric David <cedric.david@btopenworld.com>
2018-09-25 01:26:14 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts/:id', to: 'posts#show'
+  resources :posts, only: :show
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
